0c72870d78a0961ec4e9010ae73d87a160ab65b0
H-tech473 <78791161+H-tech473@users.noreply.github.com>
2023-12-23 15:35:54 +0530

    Adding fastapi postgres server

--- app/database.py
+++ app/database.py
@@ -3,9 +3,10 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from .config import settings
 
-SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
+SQLALCHEMY_DATABASE_URL = f'{settings.URL}'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_size=10, max_overflow=20)
+
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
